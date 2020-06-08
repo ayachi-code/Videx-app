5dd6210ffd4a86ab194ac59544e5b803e7017128
Bilal ayachi <bilal.ayachi@hetmml.nl>
2020-06-08 14:25:53 +0200

    kijk list css gemaakt en gestyled

--- client/src/components/watchlist.js
+++ client/src/components/watchlist.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import './style/watch.css'
+
 
 
 class Movielist extends React.Component {
     render() {
+        
         return(
-            <div>
-                hey
+            <div id="watch">
+                
             </div>
         )
     }
--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Search from './components/search';
 import Display from './components/display'
+import Movielist from './components/watchlist';
 
 class App extends React.Component {
     render() {
         return(
             <div>
                 <Search/>
+                <Movielist/>
             </div>
         )
     }
