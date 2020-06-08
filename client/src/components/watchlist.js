d3184725fab9cf7e9c8f19ef6bcbbd6e1f1f2236
Bilal ayachi <bilal.ayachi@hetmml.nl>
2020-06-08 14:13:45 +0200

    watch list component gemaakt

---
+++ client/src/components/watchlist.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+class Movielist extends React.Component {
+    render() {
+        return(
+            <div>
+                hey
+            </div>
+        )
+    }
+}
+
+export default Movielist;
