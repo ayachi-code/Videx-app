a9beffdc481ddebffb8e8343849779389c8368a5
Bilal ayachi <bilal.ayachi@hetmml.nl>
2020-06-17 13:45:22 +0200

    Feedback word in de server verwerkt en dan verstuurd na cfirebase

--- api/routes/feedbackAPI.js
+++ api/routes/feedbackAPI.js
@@ -1,13 +1,22 @@
 var express = require("express");
 var router = express.Router();
-var fetch = require("node-fetch");
+var admin = require('firebase-admin')
+
+var service = require('./videx.json')
+
+admin.initializeApp({
+    credential: admin.credential.cert(service),
+    databaseURL: "https://videx-b7759.firebaseio.com"
+
+});
 
 
 router.post("/", (req,res) => {
-    //console.log(JSON.parse(req.body))
-    console.log(Object.keys(req.body))
-    //console.log(req.body)
-
+    let data = {
+        feed: Object.keys(req.body)
+    }
+    let feedbackBase = admin.database().ref('Tips/')
+    feedbackBase.push(data)
 })
 
 module.exports = router;
