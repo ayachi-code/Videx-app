978b2e7ddea35333641e2db4774e1bafae5aadf6
Bilal ayachi <bilal.ayachi@hetmml.nl>
2020-06-17 12:43:21 +0200

    post request na server

--- client/src/components/feedback.js
+++ client/src/components/feedback.js
@@ -7,9 +7,17 @@ class Feedback extends React.Component {
         super(props)
     }
 
-    feedback() {
+    async feedback() {
         //Vraagt om de gebruikers feedback en stuurt het dan naar de server via een POST request
         let feeddback = window.prompt("Type hier je feedback :)") 
+        const reqeust = await fetch("http://localhost:9000/feedback/", {
+            method: "POST",
+            headers: {'Content-Type':'application/x-www-form-urlencoded'},
+            body: JSON.stringify(feedback)
+        })
+        const response = await reqeust.json()
+        console.log(response)
+
     }
 
     render() {
